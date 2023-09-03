61a16d64351ebffe1ca4662fda5bfed370092b24
ST <thornton.suzy@gmail.com>
2023-09-03 15:51:37 -0400

    update contact html and php

--- contact.php
+++ contact.php
@@ -1,3 +1,46 @@
+        <?php
+          // Set-up these 3 parameters
+          // 1. Enter the email address you would like the enquiry sent to
+          // 2. Enter the subject of the email you will receive, when someone contacts you
+          // 3. Enter the text that you would like the user to see once they submit the contact form
+          $to = thornton.suzy@gmail.com;
+          $subject = 'Enquiry from personal website';
+          $contact_submitted = 'Your message has been sent.';?>
+
+          <?php 
+          // Do not amend anything below here, unless you know PHP
+          function email_is_valid($email) {
+            return preg_match('/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i',$email);
+          };?>
+          <?php 
+          if (!email_is_valid($to)) {
+            echo '<p style="color: red;">You must set-up a valid (to) email address before this contact page will work.</p>';
+          };?>
+          <?php
+          if (isset($_POST['contact_submitted'])) {
+            $return = "\r";
+            $youremail = trim(htmlspecialchars($_POST['your_email']));
+            $yourname = stripslashes(strip_tags($_POST['your_name']));
+            $yourmessage = stripslashes(strip_tags($_POST['your_message']));
+            $contact_name = "Name: ".$yourname;
+            $message_text = "Message: ".$yourmessage;
+            $user_answer = trim(htmlspecialchars($_POST['user_answer']));
+            $answer = trim(htmlspecialchars($_POST['answer']));
+            $message = $contact_name . $return . $message_text;
+            $headers = "From: ".$youremail;
+            if (email_is_valid($youremail) && !eregi("\r",$youremail) && !eregi("\n",$youremail) && $yourname != "" && $yourmessage != "" && substr(md5($user_answer),5,10) === $answer) {
+              mail($to,$subject,$message,$headers);
+              $yourname = '';
+              $youremail = '';
+              $yourmessage = '';
+              echo '<p style="color: blue;">'Your message has been sent.'</p>';
+            }
+            else echo '<p style="color: red;">Please enter your name, a valid email address, your message and the answer to the simple maths question before sending your message.</p>';
+          };?>
+          
+          <?php $number_1 = rand(1, 9);?>
+          <?php $number_2 = rand(1, 9);?>
+          <?php $answer = substr(md5($number_1+$number_2),5,10);?>
 <!DOCTYPE HTML>
 <html>
 
@@ -20,8 +63,8 @@
           <li><a href="index.html">home</a></li>
           <li><a href="about.html">about me</a></li>
           <li><a href="portfolio.html">my portfolio</a></li>
-          <li><a href="fun.html">just for fun</a></li> 
-          <li class="current"><a href="contact.php">contact</a></li>
+         <!-- <li><a href="blog.html">blog</a></li> -->
+          <li class="current"><a href="contact.html">contact</a></li>
         </ul>
       </nav>
     </header>
@@ -31,7 +74,7 @@
           <ul class="images">
             <li class="show"> <!--<img width="450" height="450" src="images/1.jpg" alt="photo_one" /></li>
             <li><img width="450" height="450" src="images/2.jpg" alt="photo_two" /></li> -->
-            <li><img width="450" height="450" src="images/3.jpg" alt="photo_three" /></li>
+            <li><img width="450" src="images/3.jpg" alt="photo_three" /></li>
             <!-- <li><img width="450" height="450" src="images/4.jpg" alt="photo_four" /></li>
             <li><img width="450" height="450" src="images/5.jpg" alt="photo_five" /></li> -->
           </ul>
@@ -39,21 +82,23 @@
       </div>
       <div id="content">
         <h1>Contact</h1>
-        <form id="contact" action="contact.php" method="post">
+
+        
+        <form id="contact" action="contact.html" method="post">
           <div class="form_settings">
-            <form action="submit.php" method="POST">
-              <p>Name</p> <input type="text" name="name" class="input">
-              <p>Email</p> <input type="email" name="email" class="input">
-              <p>Message</p><textarea name="message" rows="6" cols="25" class="input"></textarea><br />
-              <input type="submit" value="Send">
-            </form>
+            <p><span>Name</span><input class="contact" type="text" name="your_name" value=" " /></p>
+            <p><span>Email Address</span><input class="contact" type="text" name="your_email" value=" " /></p>
+            <p><span>Message</span><textarea class="contact textarea" rows="5" cols="50" name="your_message"></textarea></p>
+            <p style="padding: 10px 0; line-height: 2em;">To help prevent spam, please enter the answer to this question:</p>
+            <p><span><?php echo $number_1;?> + <?php echo $number_2;?> = ?</span><input type="text" name="user_answer" /><input type="hidden" name="answer" value="<?php echo $answer; ?>" /></p>
+            <p style="padding-top: 15px"><span>&nbsp;</span><input class="submit" type="submit" name="contact_submitted" value="send" /></p>
           </div>
         </form>
       </div>
     </div>
     <footer>
-      <p><a href="index.html">home</a> | <a href="about.html">about me</a> | <a href="portfolio.html">my portfolio</a> | <a href="fun.html">just for fun</a> --> | <a href="contact.php">contact</a></p>
-      <p>&copy; 2012 my portfolio two. All Rights Reserved. | <a href="http://www.css3templates.co.uk">design from css3templates.co.uk</a></p>
+      <p><a href="index.html">home</a> | <a href="about.html">about me</a> | <a href="portfolio.html">my portfolio</a> <!-- | <a href="blog.html">blog</a> --> | <a href="contact.html">contact</a></p>
+      <p>&copy; 2023 Suzanne Thornton. All Rights Reserved. | <a href="http://www.css3templates.co.uk">design from css3templates.co.uk</a></p>
     </footer>
   </div>
   <!-- javascript at the bottom for fast page loading -->
